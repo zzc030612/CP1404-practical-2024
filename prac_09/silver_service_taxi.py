95a301afafbc3f725453ffa635a726f3ed86e52d
zzc030612 <2683332593@qq.com>
2024-11-23 00:29:14 +0800

    Add SilverServiceTaxi class with fanciness multiplier and flagfall

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class
+"""
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi instance."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Return the price for the trip with flagfall."""
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
